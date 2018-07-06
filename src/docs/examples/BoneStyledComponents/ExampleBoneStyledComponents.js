d480b5e44f27f03c3682133b30daa7f9d0c05318
ToddWebDev <webdevtodd@gmail.com>
2018-07-06 15:40:35 -0500

    Break a bone

--- src/docs/examples/BoneStyledComponents/ExampleBoneStyledComponents.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import BoneStyledComponents from "react-loading-skeleton/BoneStyledComponents";
-
-/** Little Bone using StyledComponents */
-export default function ExampleBone() {
-  return <BoneStyledComponents />;
-}
